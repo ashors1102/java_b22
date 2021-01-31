e9db92af61e1f2cc40b105b61835cfe7a8e62904
ashors1102 <ashors1102@gmail.com>
2021-01-31 19:04:49 +0300

    Moved function "distance" from MyFirstProgram as method "distance" in class Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,14 +4,17 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 
-		Point myPoint = new Point(0, 1);
+		Point myPoint = new Point(10, 1);
 		Point anotherPoint = new Point(2, 1);
-		System.out.println(distance(myPoint, anotherPoint));
+		System.out.println(myPoint.distance(myPoint, anotherPoint));
 	}
 
-	public static double distance (Point p1, Point p2) {
-		double s;
-		s = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p2.y)*(p2.y - p2.y)));
-		return s;
-	}
+	/** Функция расчета расстояния между 2х точек по их координатам
+	 *
+	 *  public static double distance (Point p1, Point p2) {
+	 *		double s;
+	 *		s = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p2.y)*(p2.y - p2.y)));
+	 *		return s;
+	 *  }
+	 */
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,4 +8,13 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    /**
+     * Метод для расчета расстояния между 2х точек по их координатам
+     */
+
+    public double distance (Point p1, Point p2) {
+        double s = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
+        return s;
+    }
 }
