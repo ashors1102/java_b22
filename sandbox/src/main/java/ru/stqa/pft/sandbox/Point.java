b5f66359c02f46b67c261f78d37af332a2774b8c
ashors1102 <ashors1102@gmail.com>
2021-01-31 19:17:33 +0300

    Refactoring method distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -14,7 +14,6 @@ public class Point {
      */
 
     public double distance (Point p1, Point p2) {
-        double s = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
-        return s;
+        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
     }
 }
