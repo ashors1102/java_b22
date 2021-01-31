b53b2e0457927db749297d3c4ff8ed58561f372f
ashors1102 <ashors1102@gmail.com>
2021-01-31 18:48:21 +0300

    Created Point.class, added function "distance"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,5 +3,15 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+
+		Point myPoint = new Point(0, 1);
+		Point anotherPoint = new Point(2, 1);
+		System.out.println(distance(myPoint, anotherPoint));
+	}
+
+	public static double distance (Point p1, Point p2) {
+		double s;
+		s = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p2.y)*(p2.y - p2.y)));
+		return s;
 	}
 }
