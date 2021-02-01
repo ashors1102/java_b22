fc615a33a356e9326a71d1401784e4893e7a030c
ashors1102 <ashors1102@gmail.com>
2021-02-01 10:44:08 +0300

    Added PointTests.class with some tests

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testZeroDistance() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+        Assert.assertEquals(p1.distance(p1, p2), 0);
+    }
+
+    @Test
+    public void testXDistance() {
+        Point p1 = new Point(10, 0);
+        Point p2 = new Point(5, 0);
+        Assert.assertEquals(p1.distance(p1, p2), 5);
+    }
+
+    @Test
+    public void testYDistance() {
+        Point p1 = new Point(0, 20);
+        Point p2 = new Point(0, 10);
+        Assert.assertEquals(p1.distance(p1, p2), 10);
+    }
+
+    @Test
+    public void testSomeDistance() {
+        Point p1 = new Point(3, 0);
+        Point p2 = new Point(0, 4);
+        Assert.assertEquals(p1.distance(p1, p2), 5);
+    }
+}
