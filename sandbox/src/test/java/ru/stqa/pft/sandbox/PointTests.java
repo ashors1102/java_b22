b0bb6f7866eb26500515b993062b98ca93fb006a
ashors1102 <ashors1102@gmail.com>
2021-02-02 18:26:41 +0300

    commit related to method "distance" fixes

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,27 +9,27 @@ public class PointTests {
     public void testZeroDistance() {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(0, 0);
-        Assert.assertEquals(p1.distance(p1, p2), 0);
+        Assert.assertEquals(p1.distance(p2), 0);
     }
 
     @Test
     public void testXDistance() {
         Point p1 = new Point(10, 0);
         Point p2 = new Point(5, 0);
-        Assert.assertEquals(p1.distance(p1, p2), 5);
+        Assert.assertEquals(p1.distance(p2), 5);
     }
 
     @Test
     public void testYDistance() {
         Point p1 = new Point(0, 20);
         Point p2 = new Point(0, 10);
-        Assert.assertEquals(p1.distance(p1, p2), 10);
+        Assert.assertEquals(p1.distance(p2), 10);
     }
 
     @Test
     public void testSomeDistance() {
         Point p1 = new Point(3, 0);
         Point p2 = new Point(0, 4);
-        Assert.assertEquals(p1.distance(p1, p2), 5);
+        Assert.assertEquals(p1.distance(p2), 5);
     }
 }
