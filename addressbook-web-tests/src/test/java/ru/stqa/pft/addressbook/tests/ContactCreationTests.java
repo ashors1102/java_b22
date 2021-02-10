19dbaff73a61d5a5a8049052339903acb526dd2c
ashors1102 <ashors1102@gmail.com>
2021-02-10 21:28:32 +0300

    Rename AddNewContactTests.class to ContactCreationTests.class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,10 +3,10 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class AddNewContactTests extends TestBase {
+public class ContactCreationTests extends TestBase {
 
   @Test
-  public void testAddNewContact() throws Exception {
+  public void testContactCreation() throws Exception {
     app.getNavigationHelper().gotoAddNewPage();
     app.getContactHelper().fillContactForm(new ContactData("Aleksey", "Shorshin", "+79162267194", "aleksey.shorshin@yandex.ru"));
     app.getContactHelper().submitNewContact();
