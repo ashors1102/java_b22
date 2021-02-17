48a1dad0fb3d209c79deb9d0094014dcd180e06a
ashors1102 <ashors1102@gmail.com>
2021-02-17 21:28:35 +0300

    Added checking and executing precondition for deletion group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,13 +5,10 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
-    app.getSessionHelper().logout();
-  }
+    @Test
+    public void testGroupCreation() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        app.getSessionHelper().logout();
+    }
 }
