77589bc0d72aef33966693257c1f7cbe578f07c2
ashors1102 <ashors1102@gmail.com>
2021-02-24 22:01:06 +0300

    Refactor searching max id with Lambdas

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.security.acl.Group;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +13,28 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1", null, null);
+        GroupData group = new GroupData("test", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+/**
+ * Поиск максимального id при помощи цикла
+ * */
 
+/*
         int max = 0;
         for (GroupData g : after){
             if (g.getId() > max){
                 max = g.getId();
             }
         }
-        group.setId(max);
+*/
+
+/**
+ * Поиск максимального id при помощи lambda-выражения
+ * */
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
