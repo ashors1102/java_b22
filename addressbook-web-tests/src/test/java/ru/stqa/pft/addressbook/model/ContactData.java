7a3e24b09494fdb00adc12080667b37c330da212
ashors1102 <ashors1102@gmail.com>
2021-03-08 19:27:04 +0300

    Saving data test to file with json format

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 import java.io.File;
@@ -8,13 +9,18 @@ import java.io.File;
 public class ContactData {
     @XStreamOmitField
     private int id = Integer.MAX_VALUE;
+    @Expose
     private String firstname;
+    @Expose
     private String lastname;
+    @Expose
     private String mobilePhone;
     private String homePhone;
     private String workPhone;
     private String allPhones;
+    @Expose
     private String email;
+    @Expose
     private String group;
     private String address;
     private String email2;
