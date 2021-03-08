3b6e61645a0c388d49c483542c8dc08da3fe8bba
ashors1102 <ashors1102@gmail.com>
2021-03-08 19:05:26 +0300

    Added ability reading from xml, writing to xml with XStream library

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
 import java.io.File;
 import java.io.FileWriter;
@@ -19,6 +20,9 @@ public class ContactDataGenerator {
     @Parameter(names = "-f", description = "Target file")
     public String file;
 
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
     public static void main (String[] args) throws IOException {
         ContactDataGenerator generator = new ContactDataGenerator();
         JCommander jCommander = new JCommander(generator);
@@ -33,10 +37,28 @@ public class ContactDataGenerator {
 
     private void run() throws IOException {
         List<ContactData> contacts = generateContacts(count);
-        save(contacts, new File(file));
+        if (format.equals("csv")){
+            saveAsCsv(contacts, new File(file));
+        }
+        else if (format.equals("xml")){
+            saveAsXml(contacts, new File(file));
+        }
+        else {
+            System.out.println("Unrecognized format " + format);
+        }
     }
 
-    private void save(List<ContactData> contacts, File file) throws IOException {
+    private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+
+    private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (ContactData contact : contacts){
@@ -50,11 +72,14 @@ public class ContactDataGenerator {
 
     private List<ContactData> generateContacts(int count) {
         List<ContactData> contacts = new ArrayList<>();
+        File photo = new File("src/test/resources/stru.png");
         for (int i = 0; i < count; i++){
             contacts.add(new ContactData().withFirstname(String.format("Firstname %s", i))
                     .withLastname(String.format("Lastname %s", i))
                     .withMobile(String.format("+7-999-99-9%s", i))
-                    .withEmail(String.format("email-%s@mail.com", i)));
+                    .withEmail(String.format("email-%s@mail.com", i))
+                    .withGroup("test1")
+                    .withPhoto(photo));
         }
         return contacts;
     }
