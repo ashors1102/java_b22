e6fd9b59945fd45111533d01e3e56886fbf2123a
ashors1102 <ashors1102@gmail.com>
2021-03-08 16:59:29 +0300

    Refactor generators with JCommander

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,12 +13,27 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-    public static void main (String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
 
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main (String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);
-        save(groups, file);
+        save(groups, new File(file));
     }
 
     private static void save(List<GroupData> groups, File file) throws IOException {
