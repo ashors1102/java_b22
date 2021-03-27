831bcef81db3786848b426ea952512d27793a1d0
ashors1102 <ashors1102@gmail.com>
2021-03-27 21:54:10 +0300

    Восстановление работоспособности модуля mantisbt
    + Поправлен loginTest

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -1,14 +1,14 @@
-<&php
-$g_hostname = 'localhost';
-$g_db_type = 'mysql';
-$g_database_name = 'bugtracker';
-$g_db_username = 'root';
-$g_db_password = '';
-$g_signup_use_captcha = OFF;
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_signup_use_captcha     = OFF;
 
-$g_default_timezone = 'Europe/Berlin';
+$g_default_timezone       = 'Europe/Berlin';
 
-$g_crypto_master_salt = 'hdmWdGR/aYbpzFI88GDHXzsYUFsVKTb5BZGQM3hXMvA=';
+$g_crypto_master_salt     = 'Ov4Bgshz3LrD83Io+oUddNPBcq9VcUVldAeVRfPg5H4=';
 
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
